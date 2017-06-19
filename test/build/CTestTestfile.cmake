ced2c4598009836653b5a18f8816d61ea0fa1087
matheuspf <matheuspff@gmail.com>
2017-06-19 13:13:21 -0300

    Fixing Covng Coveralls

--- test/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/matheus/Algoritmos/ZipIter/test
-# Build directory: /home/matheus/Algoritmos/ZipIter/test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test1 "ZipIterTests")
-subdirs(gtest)
